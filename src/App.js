0a668ab72b6f998b717161492c8c5f06fd10661f
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2020-01-16 20:49:51 -0600

    Static page implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'rbx/index.css';
+import styled from 'styled-components'
+import { Image, Column, Button } from 'rbx';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Container from '@material-ui/core/Container';
 
-function App() {
+const ProductInfo = styled.div
+`
+  text-align: center;
+`
+
+const PageTitle = styled.div
+`
+  text-align: center;
+  font-size: 20px;
+  padding: 20px;
+`
+
+const App = () => {
+  const [data, setData] = useState({});
+  const products = Object.values(data);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./data/products.json');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <PageTitle>my jank shopping cart</PageTitle>
+      <Column.Group multiline>
+        {products.map(product => <Column size="one-quarter" key={product.sku}>
+            <Card>
+              <CardContent>
+              <ProductInfo>
+              <img src={'./static/products/'+ product.sku + '_1.jpg'}/>
+                {product.title}
+                <br/>
+                ${product.price}
+                <br/>
+                <Button>S</Button>
+                <Button>M</Button>
+                <Button>L</Button>
+                </ProductInfo>
+              </CardContent>
+            </Card>
+          </Column>)}
+      </Column.Group>
+    </Container>
   );
-}
+};
 
 export default App;
