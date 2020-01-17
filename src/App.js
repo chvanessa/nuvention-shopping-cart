77842cd324c1cc78349d1498ee1fdb8f0cc47c15
Vanessa Chu <vanessachu2020@u.northwestern.edu>
2020-01-16 21:32:53 -0600

    Refactored productlist

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import { Image, Column, Button } from 'rbx';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core/styles';
+import './App.css';
 
 const ProductInfo = styled.div
 `
   text-align: center;
+  font-size: 15px;
+  padding: 10px;
 `
 
 const PageTitle = styled.div
@@ -17,6 +21,33 @@ const PageTitle = styled.div
   font-size: 20px;
   padding: 20px;
 `
+
+const handlePrices = (price) => {
+  return price.toFixed(2);
+}
+
+
+const ProductList = ({products}) => {
+  const sizes = ['S', 'M', 'L','XL']
+  return (
+    <Column.Group multiline>
+    {products.map(product => <Column size="one-quarter" key={product.sku}>
+        <Card>
+          <CardContent>
+          <img src={'./static/products/'+ product.sku + '_1.jpg'}/>
+          <ProductInfo>
+            {product.title}
+            <br/>
+            <h6>${handlePrices(product.price)}</h6>
+            <br/>
+            {sizes.map(size => <Button>{size}</Button>)}
+            </ProductInfo>
+          </CardContent>
+        </Card>
+      </Column>)}
+  </Column.Group>
+  )
+}
 
 const App = () => {
   const [data, setData] = useState({});
@@ -33,24 +64,7 @@ const App = () => {
   return (
     <Container>
       <PageTitle>my jank shopping cart</PageTitle>
-      <Column.Group multiline>
-        {products.map(product => <Column size="one-quarter" key={product.sku}>
-            <Card>
-              <CardContent>
-              <ProductInfo>
-              <img src={'./static/products/'+ product.sku + '_1.jpg'}/>
-                {product.title}
-                <br/>
-                ${product.price}
-                <br/>
-                <Button>S</Button>
-                <Button>M</Button>
-                <Button>L</Button>
-                </ProductInfo>
-              </CardContent>
-            </Card>
-          </Column>)}
-      </Column.Group>
+      <ProductList products={products}></ProductList>
     </Container>
   );
 };
